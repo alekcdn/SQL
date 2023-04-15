443e899e3573a6f16b1b319f2cff1d293ba0a8b2
alekcdn <108403184+alekcdn@users.noreply.github.com>
2023-04-15 17:33:31 +0300

    Update HomeWork2.sql

--- HomeWork2.sql
+++ HomeWork2.sql
@@ -52,6 +52,6 @@ SELECT * ,
 	CASE 
 		WHEN order_status = 'OPEN' THEN 'Order is in open state'
 		WHEN order_status = 'CLOSED' THEN 'Order is closed'
-        WHEN order_status = 'CANCELLED' THEN 'Order is cancelled'
+                WHEN order_status = 'CANCELLED' THEN 'Order is cancelled'
 	END AS 'full_order_status'	
 FROM orders;
